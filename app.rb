26ad669c60dc872dc8543d32d0533e2495b155f4
brettnelson12 <brett.nelson@kellogg.northwestern.edu>
2020-02-23 00:05:33 +0000

    Project #3 - v2

--- app.rb
+++ app.rb
@@ -9,6 +9,13 @@ before { puts "Parameters: #{params}" }
 # enter your Dark Sky API key here
 ForecastIO.api_key = "9863419f1d57fef327f7a0e19efad68b"
 
+url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=d55a2ab8a4a249799cbd868d319821e5"
+news = HTTParty.get(url).parsed_response.to_hash
+# news is now a Hash you can pretty print (pp) and parse for your output
+
+current_news = news["articles"] # continue to parse thru the news
+puts "Today's news #{current_news}"
+
 get "/" do
   # show a view that asks for the location
   view "ask"

fbed8927f523c9c5a192ab9af8182035e2529ac4
brettnelson12 <brett.nelson@kellogg.northwestern.edu>
2020-02-23 23:56:44 +0000

    project #3 - v3

--- app.rb
+++ app.rb
@@ -9,12 +9,12 @@ before { puts "Parameters: #{params}" }
 # enter your Dark Sky API key here
 ForecastIO.api_key = "9863419f1d57fef327f7a0e19efad68b"
 
+# News API
 url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=d55a2ab8a4a249799cbd868d319821e5"
-news = HTTParty.get(url).parsed_response.to_hash
+@news = HTTParty.get(url).parsed_response.to_hash
+  @each_article = []
+  
 # news is now a Hash you can pretty print (pp) and parse for your output
-
-current_news = news["articles"] # continue to parse thru the news
-puts "Today's news #{current_news}"
 
 get "/" do
   # show a view that asks for the location
@@ -24,7 +24,22 @@ end
 get "/news" do
   # do everything else
     results = Geocoder.search(params["q"])
-    lat_long = results.first.coordinates # => [lat, long]
-    "#{lat_long[0]} #{lat_long[1]}"
-    view "ask"
+    @lat_long = results.first.coordinates # => [lat, long]
+    @location = results.first.city
+    # Define the lat and long
+    @lat = "#{@lat_long [0]}"
+    @long = "#{@lat_long [1]}"
+
+    # Results from Geocoder
+    @forecast = ForecastIO.forecast("#{@lat}" , "#{@long}").to_hash
+    @current_temperature = @forecast["currently"]["temperature"]
+    @current_conditions = @forecast["currently"]["summary"]
+# high_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
+# puts high_temperature
+# puts forecast["daily"]["data"][1]["temperatureHigh"]
+# puts forecast["daily"]["data"][2]["temperatureHigh"]
+    @daily_temperature = []
+    @daily_conditions = []
+    @daily_wind = []
+    @daily_humidity = []
 end
